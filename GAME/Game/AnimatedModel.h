e705b6265aedce6a1644d2e00a60a9058cc8145b
jos_boon <josejboon@gmail.com>
2015-03-02 08:16:29 +0100

    Basic player movement (without use of bullet)

--- GAME/Game/AnimatedModel.h
+++ GAME/Game/AnimatedModel.h
@@ -13,6 +13,7 @@ public:
 	AnimatedModel(ISceneManager* smgr, IVideoDriver* driver, io::path meshName, io::path textureName, vector3df position, vector3df rotation);
 	AnimatedModel(ISceneManager* smgr, IVideoDriver* driver, io::path meshName, io::path textureName, vector3df position, vector3df rotation, vector3df scale);
 	void Initialize(ISceneManager* smgr, IVideoDriver* driver, io::path meshName, io::path textureName);
+	void UpdatePos() { aNode->setPosition(this->getPosition()); }
 	~AnimatedModel();
 };
 
--- GAME/Game/Main.cpp
+++ GAME/Game/Main.cpp
@@ -1,5 +1,6 @@
 #include <irrlicht.h>
 #include "Player.h"
+#include "InputReceiver.h"
 
 using namespace irr;
 
@@ -9,11 +10,25 @@ using namespace video;
 using namespace io;
 using namespace gui;
 
+u32 then;
+f32 getFrameDeltaTime(IrrlichtDevice* device)
+{
+	// Work out a frame delta time.
+	const u32 now = device->getTimer()->getTime();
+	const f32 frameDeltaTime = (f32)(now - then) / 1000.f; // Time in seconds
+
+	then = now;
+
+	return frameDeltaTime;
+}
+
 int main()
 {
+	InputReceiver input;
+
 	IrrlichtDevice *device =
 		createDevice(video::EDT_OPENGL, dimension2d<u32>(640, 480), 16,
-		false, false, false, 0);
+		false, false, false, &input);
 	
 	if (!device)
 		return 1;
@@ -26,14 +41,16 @@ int main()
 
 	/*guienv->addStaticText(L"Hello World! This is the Irrlicht OPENGL renderer!",
 		rect<s32>(10, 10, 260, 22), true);*/
-
-	Player p(smgr, driver, "../Assets/sydney.md2", "../Assets/sydney.bmp", vector3df());
+	Player p(smgr, driver, &input, "../Assets/sydney.md2", "../Assets/sydney.bmp", vector3df());
 
 	smgr->addCameraSceneNode(0, vector3df(0, 30, -40), vector3df(0, 5, 0));
 
+	then = device->getTimer()->getTime();
 	while (device->run())
 	{
 		driver->beginScene(true, true, SColor(255, 100, 101, 140));
+
+		p.Update(getFrameDeltaTime(device));
 
 		smgr->drawAll();
 		guienv->drawAll();
