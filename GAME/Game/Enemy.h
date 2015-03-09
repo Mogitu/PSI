da3475254c14f45d81795cd086d16a5214c850bf
jos_boon <josejboon@gmail.com>
2015-03-09 12:08:59 +0100

    Merge branch 'master' of github.com:Mogitu/Particle_Editor_PSI into HEAD
    Conflicts:
    	GAME/Game.sdf
    	GAME/Game.v12.suo
    	GAME/Game/Game.vcxproj
    	GAME/Game/Game.vcxproj.filters

---
+++ GAME/Game/Enemy.h
@@ -0,0 +1,21 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <irrlicht.h>
+#include <iostream>
+#include <string>
+
+class Enemy
+{
+public:
+	Enemy(irr::scene::ISceneManager *smgr,irr::video::IVideoDriver *irrDriver, irr::io::path meshpath, irr::io::path texturepath , float x, float y, float z, float scalex, float scaley, float scalez);
+	~Enemy();
+	void Kill(float beginanim, float endanim, float animationspeed);
+private:
+	irr::scene::ISceneManager *smgr;
+	irr::video::IVideoDriver *irrDriver;
+	irr::scene::IAnimatedMesh* mesh;
+	irr::scene::IAnimatedMeshSceneNode* node;
+};
+
+#endif ENEMY_H
