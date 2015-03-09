0e4b0a08764c25a93659e053851aa9c462adf6d9
jos_boon <josejboon@gmail.com>
2015-03-09 08:36:37 +0100

    Cleaned up the main function
    Added Jump to Player
    Updated the player movement
    added #ifndef in Projectile class

--- GAME/Game/Main.cpp
+++ GAME/Game/Main.cpp
@@ -7,44 +7,12 @@
 #include "Projectile.h"
 #include "Player.h"
 
-
-
 using namespace irr;
 using namespace core;
 using namespace scene;
 using namespace video;
 using namespace io;
 using namespace gui;
-
-class MyEventReceiver : public IEventReceiver
-{
-public:
-	// This is the one method that we have to implement
-	virtual bool OnEvent(const SEvent& event)
-	{
-		// Remember whether each key is down or up
-		if (event.EventType == irr::EET_KEY_INPUT_EVENT)
-			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
-
-		return false;
-	}
-
-	// This is used to check whether a key is being held down
-	virtual bool IsKeyDown(EKEY_CODE keyCode) const
-	{
-		return KeyIsDown[keyCode];
-	}
-
-	MyEventReceiver()
-	{
-		for (u32 i = 0; i<KEY_KEY_CODES_COUNT; ++i)
-			KeyIsDown[i] = false;
-	}
-
-private:
-	// We use this array to store the current state of each key
-	bool KeyIsDown[KEY_KEY_CODES_COUNT];
-};
 
 int main() {
 
@@ -60,7 +28,6 @@ int main() {
 	Level *level;
 	BulletHelper* helper;
 	InputReceiver* input = new InputReceiver();
-	MyEventReceiver receiver;
 
 	// Initialize irrlicht	
 	device = createDevice(video::EDT_OPENGL, dimension2d<u32>(800, 600), 32, false, false, false, input);
@@ -72,7 +39,7 @@ int main() {
 
 	// Add camera
 	ICameraSceneNode *camera = smgr->addCameraSceneNodeFPS(0, 100, 0.1);
-	//camera->setPosition(vector3df(-161, 100, -300));
+	camera->setPosition(vector3df(-161, 100, -150));
 	camera->setTarget(vector3df(0, 0, 0));
 	
 	level = new Level(smgr, "../Assets/level.irr");	
@@ -82,32 +49,12 @@ int main() {
 
 	helper = new BulletHelper();
 	helper->buildIrrLevel(level);
-	
-	//Default mesh from demos
-	IMesh* mesh = smgr->getMesh("../Assets/sydney.md2");
 
-	//default node setup
-	IMeshSceneNode* node = smgr->addMeshSceneNode(mesh);
-	node->setMaterialTexture(0, irrDriver->getTexture("../Assets/sydney.bmp"));
-	node->setPosition(vector3df(0, 100, 80));
-
-	Projectile *p = new Projectile(smgr, helper);
-
-	//createa a new rigidbody from earlier made node
-	btRigidBody *b =helper->createBody(node,Shape_Type::CAPSULE, 10000);	
-
-	camera->setTarget(node->getPosition());
-
-	b->setAngularFactor(btVector3(0,1,0));
-	b->setInterpolationLinearVelocity(btVector3(100,0,0));
-	b->setRestitution(0);
-	b->setFriction(2);
-	
-
-
+	//Create the game world
 	GameWorld* gWorld = new GameWorld();
-	Player* p = new Player(smgr, irrDriver, helper, input, "../Assets/sydney.md2", "../Assets/sydney.bmp", Shape_Type::CAPSULE, 1, vector3df(0, 100, 0));
-	gWorld->addGameObject(p);
+	Player* player = new Player(smgr, irrDriver, helper, gWorld, input, "../Assets/sydney.md2", "../Assets/sydney.bmp", Shape_Type::CAPSULE, 80, vector3df(0, 100, 0));
+
+	camera->setTarget(player->getNodePosition());
 	
 	// Main loop
 	u32 timeStamp = irrTimer->getTime(), deltaTime = 0;
@@ -124,51 +71,9 @@ int main() {
 		guiEnv->drawAll();
 		irrDriver->endScene();
 
+		//Close Device
 		if (input->IsKeyDown(EKEY_CODE::KEY_ESCAPE))
 			device->closeDevice();
-		//Update input and player
-		if (b)
-		{
-			b->setAngularVelocity(btVector3(0, 0, 0));
-			if (receiver.IsKeyDown(irr::KEY_KEY_W))
-			{
-				b->applyCentralImpulse(helper->extractForwardVector(b) * 2000);
-			}
-			else if (receiver.IsKeyDown(irr::KEY_KEY_S))
-			{
-
-				b->applyCentralImpulse(-helper->extractForwardVector(b) * 2000);
-			}
-
-			if (receiver.IsKeyDown(irr::KEY_KEY_A))
-			{
-
-				b->setAngularVelocity(btVector3(0, -5, 0));
-			}
-			else if (receiver.IsKeyDown(irr::KEY_KEY_D))
-			{
-				b->setAngularVelocity(btVector3(0, 5, 0));
-			}
-
-			if (receiver.IsKeyDown(irr::KEY_KEY_Q))
-			{
-				b->applyCentralImpulse(btVector3(0, 6000, 0));
-			}
-
-			if (receiver.IsKeyDown(irr::KEY_KEY_E))
-			{				
-				p->fire(b->getWorldTransform().getOrigin(), helper->extractForwardVector(b));
-			}
-			if (receiver.IsKeyDown(irr::KEY_KEY_P))
-			{		
-					p->kill();			
-			}
-
-			if (receiver.IsKeyDown(irr::KEY_KEY_O))
-			{
-				p->resurrect();
-			}
-		}	
 	}
 	device->drop();
 	return 0;
--- GAME/Game/Player.h
+++ GAME/Game/Player.h
@@ -6,6 +6,9 @@
 #include <btBulletCollisionCommon.h>
 #include "BulletHelper.h"
 #include "IGameObject.h"
+#include "BulletHelper.h"
+#include "GameWorld.h"
+#include "Projectile.h"
 
 class Player : public IGameObject
 {
@@ -13,14 +16,21 @@ private:
 	InputReceiver* input;
 	btRigidBody* body;
 	scene::IAnimatedMeshSceneNode* node;
+	BulletHelper* helper;
+	ISceneManager* smgr;
+	Projectile *p;
+	vector2di oldMousePos;
 
-
+	bool justJumped;
+	bool isGrounded();
 public:
-	Player(ISceneManager* smgr, IVideoDriver* driver, BulletHelper* helper, InputReceiver* input, io::path meshName, io::path textureName, Shape_Type bodyType, btScalar bodyMass,vector3df position = vector3df(0, 0, 0), vector3df rotation = vector3df(0, 0, 0), vector3df scale = vector3df(1, 1, 1));
+	Player(ISceneManager* smgr, IVideoDriver* driver, BulletHelper* helper, GameWorld* world,InputReceiver* input, io::path meshName, io::path textureName, Shape_Type bodyType, btScalar bodyMass = 1,vector3df position = vector3df(0, 0, 0), vector3df rotation = vector3df(0, 0, 0), vector3df scale = vector3df(1, 1, 1));
 	void Initialize();
-	void Initialize(ISceneManager* smgr, IVideoDriver* driver, BulletHelper* helper, InputReceiver* input, io::path meshName, io::path textureName, Shape_Type bodyType, btScalar bodyMass, vector3df position = vector3df(0, 0, 0), vector3df rotation = vector3df(0, 0, 0), vector3df scale = vector3df(1, 1, 1));
+	void Initialize(ISceneManager* smgr, IVideoDriver* driver, BulletHelper* helper, GameWorld* world, InputReceiver* input, io::path meshName, io::path textureName, Shape_Type bodyType, btScalar bodyMass, vector3df position = vector3df(0, 0, 0), vector3df rotation = vector3df(0, 0, 0), vector3df scale = vector3df(1, 1, 1));
 	void Update(u32 frameDeltaTime);
 	void PlayerMovement(u32 frameDeltaTime);
+	void Fire();
+	vector3df getNodePosition();
 	GameObjectType getType();
 };
 
