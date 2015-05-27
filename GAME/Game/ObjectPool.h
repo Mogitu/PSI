6c4180820ee03e2204a43b12cbdbc83db02f840b
Maikel <mogitu@msn.com>
2015-05-27 21:29:40 +0200

    *edit in de pool

--- GAME/Game/ObjectPool.h
+++ GAME/Game/ObjectPool.h
@@ -1,25 +1,32 @@
 #ifndef OBJECTPOOL_H
 #define OBJECTPOOL_H
 
+
+/*
+*Author: Maikel van Munsteren
+*Desc: Easy to use object pool that can be used for pooling objects of any type.
+*      Features fast (de-)allocation in O(1) by using a free list.
+*/
+
 template <class T>
-class ObjectPool{	
+class ObjectPool{
 private:
+	//nested class to keep track of each objects status in a decoupled way.
 	struct Object{
 		bool available;
 		T data;
 		Object *next;
 	};
-public:	
-	ObjectPool(int size);
-	~ObjectPool();		
-	T *create();	
-	Object *getObjects();
-	int getSize();
-	void returnToPool(Object *obj);	
-private:	
 	Object *firstAvailable;
 	Object *objects;
 	int size;
+public:
+	ObjectPool(int size);
+	~ObjectPool();
+	T *create();
+	Object *getObjects();
+	int getSize();
+	void returnToPool(Object *obj);
 };
 
 template <typename T>
@@ -36,15 +43,19 @@ int ObjectPool<T>::getSize()
 
 
 template <typename T>
-ObjectPool<T>::ObjectPool(int s){	
+ObjectPool<T>::ObjectPool(int s){
 	size = s;
+	//create an array of objects.
 	objects = new Object[size];
+	//first object created is immediatly available.
 	firstAvailable = &objects[0];
+	//make all objects available by default and give them a reference to the next object.
 	for (int i = 0; i < size; i++)
 	{
 		objects[i].available = true;
 		objects[i].next = &objects[i + 1];
-	}	
+	}
+	//the last object in the list has no next, list will terminate here.
 	objects[size - 1].next = NULL;
 }
 
@@ -55,22 +66,27 @@ ObjectPool<T>::~ObjectPool(){
 }
 template<typename T>
 T *ObjectPool<T>::create(){
+	//get a pointer to the first object of T from the Object.
 	T *obj = &firstAvailable->data;
+	//if succesfull it is no longer available 
 	if (obj && firstAvailable->available)
 	{
 		firstAvailable->available = false;
+		//change the next firstavailable object of T
 		if (firstAvailable->next)
 		{
 			firstAvailable = firstAvailable->next;
-		}		
+		}
 		return obj;
 	}
-	return NULL;	
+	//if creation fails.
+	return NULL;
 }
 
 template<typename T>
 void ObjectPool<T>::returnToPool(Object *obj)
 {
+	//object is available again and we set the references to the next and firstavailable objects again.
 	obj->available = true;
 	obj->next = firstAvailable;
 	firstAvailable = obj;
